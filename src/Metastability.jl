a270cd933b0ac20ff9b51ccd1bc3cbc54afb4f3f
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-23 13:04:41 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.44

---
+++ src/Metastability.jl
@@ -0,0 +1,5 @@
+module Metastability
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Metastability
+using Test
+
+@testset "Metastability.jl" begin
+    # Write your tests here.
+end
